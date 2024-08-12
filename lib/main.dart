1242e260e4f0e7e7de270bdf622358e15765afab
duckye <161498434+DagmawiIsaiah@users.noreply.github.com>
2024-08-12 19:05:44 +0300

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 
+import '../utils/utils.dart';
+import '../screens/screens.dart';
+
 void main() {
   runApp(const PSSSA());
 }
 
 class PSSSA extends StatelessWidget {
   const PSSSA({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'PSSSA',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
+      theme: AppTheme.lightTheme,
+      initialRoute: LoginScreen.routeName,
+      routes: {
+        LoginScreen.routeName: (context) => const LoginScreen(),
+        HomeScreen.routeName: (context) => const HomeScreen(),
+        AdminHomeScreen.routeName: (context) => const AdminHomeScreen(),
+      },
     );
   }
 }
