3cd83aeb48c9cc443340994782da8789abc54756
duckye <161498434+DagmawiIsaiah@users.noreply.github.com>
2024-08-12 19:02:54 +0300

    create home screen

---
+++ lib/screens/home.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/utils.dart';
+import '../widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = '/home';
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Header(),
+            const Divider(),
+            const SizedBox(height: SpacingSize.s24),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 150,
+                vertical: 24,
+              ),
+              child: Text(
+                "Filter and Search",
+                style: textTheme.headlineLarge,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 150,
+              ),
+              child: Row(
+                children: [
+                  const SizedBox(
+                    width: 300,
+                    child: CupertinoSearchTextField(),
+                  ),
+                  const SizedBox(
+                    width: SpacingSize.s20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Search"),
+                  ),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 150,
+                vertical: 24,
+              ),
+              child: Row(
+                children: [
+                  Text("Filter by"),
+                  SizedBox(width: SpacingSize.s16),
+                  DropdownMenu(
+                    label: Text("Category"),
+                    dropdownMenuEntries: [
+                      DropdownMenuEntry(value: 0, label: "Civil"),
+                      DropdownMenuEntry(value: 1, label: "Military"),
+                      DropdownMenuEntry(value: 2, label: "Police"),
+                      DropdownMenuEntry(value: 3, label: "Undertaking"),
+                    ],
+                  ),
+                  SizedBox(width: SpacingSize.s16),
+                  DropdownMenu(
+                    label: Text("Region"),
+                    dropdownMenuEntries: [
+                      DropdownMenuEntry(value: 0, label: "Civil"),
+                      DropdownMenuEntry(value: 1, label: "Military"),
+                      DropdownMenuEntry(value: 2, label: "Police"),
+                      DropdownMenuEntry(value: 3, label: "Undertaking"),
+                    ],
+                  ),
+                  SizedBox(width: SpacingSize.s16),
+                  DropdownMenu(
+                    label: Text("City"),
+                    dropdownMenuEntries: [
+                      DropdownMenuEntry(value: 0, label: "Civil"),
+                      DropdownMenuEntry(value: 1, label: "Military"),
+                      DropdownMenuEntry(value: 2, label: "Police"),
+                      DropdownMenuEntry(value: 3, label: "Undertaking"),
+                    ],
+                  ),
+                  SizedBox(width: SpacingSize.s16),
+                  DropdownMenu(
+                    label: Text("Status"),
+                    dropdownMenuEntries: [
+                      DropdownMenuEntry(value: 0, label: "Civil"),
+                      DropdownMenuEntry(value: 1, label: "Military"),
+                      DropdownMenuEntry(value: 2, label: "Police"),
+                      DropdownMenuEntry(value: 3, label: "Undertaking"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const Record(),
+            const SizedBox(height: 10),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.45,
+              child: ListView.separated(
+                itemBuilder: (context, index) => const Record(),
+                itemCount: 15,
+                separatorBuilder: (BuildContext context, int index) {
+                  return const SizedBox(
+                    height: 5,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
