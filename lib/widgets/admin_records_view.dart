db29a38594c066cc6445c0f2cfca29f7a3df10a7
duckye <161498434+DagmawiIsaiah@users.noreply.github.com>
2024-08-19 09:02:30 +0300

    make admin records view responsive

--- lib/widgets/admin_records_view.dart
+++ lib/widgets/admin_records_view.dart
@@ -10,98 +10,91 @@ class AdminRecordsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-    
+    final textTheme = context.textTheme;
+    final size = context.deviceSize;
+
     return Expanded(
       flex: 5,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(
-              // horizontal: 150,
-              vertical: 24,
-            ),
+            padding: const EdgeInsets.symmetric(vertical: 24),
             child: Text(
-              "Filter and Search",
+              "Search",
               style: textTheme.headlineLarge,
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-                // horizontal: 150,
+          Row(
+            children: [
+              const SizedBox(
+                width: 300,
+                child: CupertinoSearchTextField(
+                  placeholder: "Enter Name or Pention Number",
                 ),
-            child: Row(
-              children: [
-                const SizedBox(
-                  width: 300,
-                  child: CupertinoSearchTextField(),
-                ),
-                const SizedBox(
-                  width: SpacingSize.s20,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text("Search"),
-                ),
-              ],
-            ),
+              ),
+              const SizedBox(
+                width: SpacingSize.s20,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("Search"),
+              ),
+            ],
           ),
-          const Padding(
-            padding: EdgeInsets.symmetric(
-              // horizontal: 150,
-              vertical: 24,
-            ),
-            child: Row(
-              children: [
-                Text("Filter by"),
-                SizedBox(width: SpacingSize.s16),
-                DropdownMenu(
-                  label: Text("Category"),
-                  dropdownMenuEntries: [
-                    DropdownMenuEntry(value: 0, label: "Civil"),
-                    DropdownMenuEntry(value: 1, label: "Military"),
-                    DropdownMenuEntry(value: 2, label: "Police"),
-                    DropdownMenuEntry(value: 3, label: "Undertaking"),
-                  ],
-                ),
-                SizedBox(width: SpacingSize.s16),
-                DropdownMenu(
-                  label: Text("Region"),
-                  dropdownMenuEntries: [
-                    DropdownMenuEntry(value: 0, label: "Civil"),
-                    DropdownMenuEntry(value: 1, label: "Military"),
-                    DropdownMenuEntry(value: 2, label: "Police"),
-                    DropdownMenuEntry(value: 3, label: "Undertaking"),
-                  ],
-                ),
-                SizedBox(width: SpacingSize.s16),
-                DropdownMenu(
-                  label: Text("City"),
-                  dropdownMenuEntries: [
-                    DropdownMenuEntry(value: 0, label: "Civil"),
-                    DropdownMenuEntry(value: 1, label: "Military"),
-                    DropdownMenuEntry(value: 2, label: "Police"),
-                    DropdownMenuEntry(value: 3, label: "Undertaking"),
-                  ],
-                ),
-                SizedBox(width: SpacingSize.s16),
-                DropdownMenu(
-                  label: Text("Status"),
-                  dropdownMenuEntries: [
-                    DropdownMenuEntry(value: 0, label: "Civil"),
-                    DropdownMenuEntry(value: 1, label: "Military"),
-                    DropdownMenuEntry(value: 2, label: "Police"),
-                    DropdownMenuEntry(value: 3, label: "Undertaking"),
-                  ],
-                ),
-              ],
-            ),
+          const SizedBox(height: SpacingSize.s20),
+          Text(
+            "Filter By",
+            style: textTheme.titleLarge,
           ),
+          const SizedBox(height: SpacingSize.s20),
+          const Wrap(
+            runSpacing: SpacingSize.s16,
+            spacing: SpacingSize.s16,
+            children: [
+              DropdownMenu(
+                label: Text("Category"),
+                dropdownMenuEntries: [
+                  DropdownMenuEntry(value: 0, label: "Civil"),
+                  DropdownMenuEntry(value: 1, label: "Military"),
+                  DropdownMenuEntry(value: 2, label: "Police"),
+                  DropdownMenuEntry(value: 3, label: "Undertaking"),
+                ],
+              ),
+              DropdownMenu(
+                label: Text("Region"),
+                dropdownMenuEntries: [
+                  DropdownMenuEntry(value: 0, label: "Civil"),
+                  DropdownMenuEntry(value: 1, label: "Military"),
+                  DropdownMenuEntry(value: 2, label: "Police"),
+                  DropdownMenuEntry(value: 3, label: "Undertaking"),
+                ],
+              ),
+              DropdownMenu(
+                label: Text("City"),
+                dropdownMenuEntries: [
+                  DropdownMenuEntry(value: 0, label: "Civil"),
+                  DropdownMenuEntry(value: 1, label: "Military"),
+                  DropdownMenuEntry(value: 2, label: "Police"),
+                  DropdownMenuEntry(value: 3, label: "Undertaking"),
+                ],
+              ),
+              DropdownMenu(
+                label: Text("Status"),
+                dropdownMenuEntries: [
+                  DropdownMenuEntry(value: 0, label: "Civil"),
+                  DropdownMenuEntry(value: 1, label: "Military"),
+                  DropdownMenuEntry(value: 2, label: "Police"),
+                  DropdownMenuEntry(value: 3, label: "Undertaking"),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: SpacingSize.s20),
           const RecordHeader(),
           const SizedBox(height: 10),
           SizedBox(
-            height: MediaQuery.of(context).size.height * 0.45,
+            height: size.height * 0.45,
             child: ListView.separated(
               itemBuilder: (context, index) => const RecordView(
                 record: Record(
